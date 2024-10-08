b243c56c9e347109e577c3d39710f2666d4162d7
Christian Moyes <339463846@GoTVDSB.ca>
2024-10-08 08:44:48 -0400

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace Monogame_Topic_2___Loops_and_Lists_Assignment
+{
+    //Christian Moyes
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Random generator = new Random();
+
+        MouseState mouseState;
+        MouseState prevMouseState;
+
+        KeyboardState keyboardState;
+        KeyboardState prevKeyboardState;
+
+        Texture2D backgroundTexture;
+        Texture2D canvasTexture;
+
+        Rectangle backgroundRect;
+        Rectangle canvasRect;
+
+        List<Texture2D> textures;
+        List<Texture2D> splatTextures;
+        List<Rectangle> splatRects;
+
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 500;
+            _graphics.ApplyChanges();
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            backgroundRect = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
+            canvasRect = new Rectangle(150, 100, 200, 400);
+
+            textures = new List<Texture2D>();
+            splatTextures = new List<Texture2D>();
+            splatRects = new List<Rectangle>();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Monogame_Topic_2___Loops_and_Lists_Assignment.Game1();
+game.Run();
